8b2f53bfd1ad529436a1b430b8d4a8e159e7181b
Willie Boag <Willie@Willie.(none)>
2014-10-11 02:45:04 -0400

    Step 2 complete

--- stem.rkt
+++ stem.rkt
@@ -27,18 +27,18 @@
     (define/public (stem w)
       (begin
         (set! successful-1b #f)    ; rest global field
-        (step1 w)))
+        (string-downcase (step2 (step1 (string-upcase w))))))
 
     
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;; Private Methods ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     
-    (define (step1 str)
+    (define/private (step1 str)
       (step1b (step1a str)))
 
     
     ;Step 1a
-    (define/public (step1a w)
+    (define/private (step1a w)
 
       ; SSES -> SS                         caresses  ->  caress
       ; IES  -> I                          ponies    ->  poni
@@ -67,7 +67,7 @@
     
     
     ;Step 1b
-    (define (step1b w)
+    (define/private (step1b w)
 
       ;(m>0) EED -> EE                    feed      ->  feed
       ;                                   agreed    ->  agree
@@ -79,7 +79,6 @@
               str
             (string-append (second eed) "EE"))))
             
-    
       ;(*v*) ED  ->                       plastered ->  plaster
       ;                                   bled      ->  bled
       (define (step1b-2 str)
@@ -92,7 +91,7 @@
               (begin 
                 (set! successful-1b #t)
                 (second eed)))))
-    
+   
       ;(*v*) ING ->                       motoring  ->  motor
       ;                                   sing      ->  sing
       (define (step1b-3 str)
@@ -108,7 +107,7 @@
               (begin
                 (set! successful-1b #t)
                 (second eed)))))
-
+      
       ;  AT -> ATE                       conflat(ed)  ->  conflate
       ;  BL -> BLE                       troubl(ed)   ->  trouble
       ;  IZ -> IZE                       siz(ed)      ->  size
@@ -137,36 +136,117 @@
                       (not (equal? final #\S))
                       (not (equal? final #\Z)))
                  (list->string (reverse (cdr (reverse (string->list str))))))
-                ; (m=1 and *o) -> E
+                ((ends-cvc? str) (string-append str "E"))
                 (else str)))
             str))
-
         
       ; Apply all sub-steps in step 1b
       (step1b-4 (step1b-3 (step1b-2 (step1b-1 w)))))
 
     
+    ; Step 2
+    (field (step2-suffixes '( ("ATIONAL" "ATE")
+                              ("TIONAL"  "TION")
+                              ("ENCI"    "ENCE")
+                              ("ANCI"    "ANCE")
+                              ("IZER"    "IZE")
+                              ("ABLI"    "ABLE")
+                              ("ALLI"    "AL")
+                              ("ENTLI"   "ENT")
+                              ("ELI"     "E")
+                              ("OUSLI"   "OUS")
+                              ("IZATION" "IZE")
+                              ("ATION"   "ATE")
+                              ("ATOR"    "ATE")
+                              ("ALISM"   "AL")
+                              ("IVENESS" "IVE")
+                              ("FULNESS" "FUL")
+                              ("OUSNESS" "OUS")
+                              ("ALITI"   "AL")
+                              ("IVITI"   "IVE")
+                              ("BILITI"  "BLE"))))
+
+    ; Step 2
+    (define (step2 w)
+      
+      ; Iterate over step2-suffixes list to find right substitution
+      (define (step2-helper s suffixes)
+        (if (null? suffixes)
+            s
+            (let
+                ((stem (regexp-match 
+                        (pregexp (string-append "^(.*)"
+                                                (caar suffixes)
+                                                "$"))
+                        s)))
+              (if (and stem                        ; match found
+                       (> (get-m (cadr stem)) 0))  ; new stem must have m>0
+                  (string-append (cadr stem) (cadar suffixes))
+                  (step2-helper s (cdr suffixes))))))
+      
+      (step2-helper w step2-suffixes))
+        
+    
+    ; (m>0) ATIONAL ->  ATE           relational     ->  relate
+    ; (m>0) TIONAL  ->  TION          conditional    ->  condition
+    ;                                 rational       ->  rational
+    ; (m>0) ENCI    ->  ENCE          valenci        ->  valence
+    ; (m>0) ANCI    ->  ANCE          hesitanci      ->  hesitance
+    ; (m>0) IZER    ->  IZE           digitizer      ->  digitize
+    ; (m>0) ABLI    ->  ABLE          conformabli    ->  conformable
+    ; (m>0) ALLI    ->  AL            radicalli      ->  radical
+    ; (m>0) ENTLI   ->  ENT           differentli    ->  different
+    ; (m>0) ELI     ->  E             vileli        - >  vile
+    ; (m>0) OUSLI   ->  OUS           analogousli    ->  analogous
+    ; (m>0) IZATION ->  IZE           vietnamization ->  vietnamize
+    ; (m>0) ATION   ->  ATE           predication    ->  predicate
+    ; (m>0) ATOR    ->  ATE           operator       ->  operate
+    ; (m>0) ALISM   ->  AL            feudalism      ->  feudal
+    ; (m>0) IVENESS ->  IVE           decisiveness   ->  decisive
+    ; (m>0) FULNESS ->  FUL           hopefulness    ->  hopeful
+    ; (m>0) OUSNESS ->  OUS           callousness    ->  callous
+    ; (m>0) ALITI   ->  AL            formaliti      ->  formal
+    ; (m>0) IVITI   ->  IVE           sensitiviti    ->  sensitive
+    ; (m>0) BILITI  ->  BLE           sensibiliti    ->  sensible
     
 
-    (define (contains-vowel? w)
+    ; does input w contain any vowels?
+    (define/private (contains-vowel? w)
       (ormap (lambda (i) (not (consonant? w i))) 
              (range 0 (string-length w))))
     
     
-    (define (consonant? str ind)
+    ; Is character w[i] a consonant?
+    (define/private (consonant? str ind)
       (not
-       (regexp-match? #rx"((?i:.(?:a|e|i|o|u)|(?:[^aeiou])y))"
+       (regexp-match? #rx"((?i:(?<=.)(?i:a|e|i|o|u)|(?<=[^aeiou])y))"
                       (substring str 
                                  (max 0 (- ind 1)) 
                                  (+ 1 ind)))))
 
     
-    (define (ends-double-c? w)
+    ; Does character end with a repeated consonant?
+    (define/private (ends-double-c? w)
       (regexp-match #px"^.*([^aeiouy])\\1$" w))
 
+
+    ; Does the stem end with cvc, where the second c is not W, X or Y ?
+    (define/private (ends-cvc? w)
+      (let*
+          ((n (string-length w))
+           (c (char-upcase (string-ref w (- n 1)))))
+        (and
+         (> n 2)
+         (     consonant? w (- n 1) )
+         (not (consonant? w (- n 2)))
+         (     consonant? w (- n 3) )
+         (not (equal? c #\W))
+         (not (equal? c #\X))
+         (not (equal? c #\Y)))))
+
       
-      
-    (define/public (get-m s)
+    ; Get "measure of string 
+    (define/private (get-m s)
       ; Number of inner VC pairs => half of the length of the inner list
       (- (/ (length (CV-split s)) 2) 1))
     
@@ -175,7 +255,7 @@
     ; Note: Always begins with consontant strng & ends with vowel string
     ;                        C   V   C    V   C    V   C   V   C   V
     ; ex. "alternating" => '("" "a" "lt" "e" "rn" "a" "t" "i" "ng" "")
-    (define/public (CV-split str)
+    (define/private (CV-split str)
       
       ; "alterating" => '("" "a" "lternating")
       (define (get-leading-CV s)
@@ -228,16 +308,38 @@
 ;                "MOTORING"
 ;                "SING"))
 
-(define words '("CONFLATED"
-                "TROUBLED"
-                "SIZED"
-                "HOPPING"
-                "TANNED"
-                "FALLING"
-                "HISSING"
-                "FIZZED"
-                "FAILING"
-                "FILING"))
+;(define words '("CONFLATED"
+;                "TROUBLED"
+;                "SIZED"
+;                "HOPPING"
+;                "TANNED"
+;                "FALLING"
+;                "HISSING"
+;                "FIZZED"
+;                "FAILING"
+;                "FILING"))
+
+(define words '("RELATIONAL"
+                "CONDITIONAL"
+                "RATIONAL"
+                "VALENCI"
+                "HESITANCI"
+                "DIGITIZER"
+                "CONFORMABLI"
+                "RADICALLI"
+                "DIFFERENTLI"
+                "VILELI"
+                "ANALOGOUSLI"
+                "VIETNAMIZATION"
+                "PREDICATION"
+                "OPERATOR"
+                "FEUDALISM"
+                "DECISIVENESS"
+                "HOPEFULNESS"
+                "CALLOUSNESS"
+                "FORMALITI"
+                "SENSITIVITI"
+                "SENSIBILITI"))
 
 (display words)
 (newline)
