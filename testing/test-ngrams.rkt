a78486f1199479a389cc3e79df4ff6ca0e27ce55
Willie Boag <william.george.boag@gmail.com>
2014-10-15 02:36:46 -0400

    Move ngram tests into separate file

---
+++ testing/test-ngrams.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+
+(require "../ngrams.rkt")
+
+
+; Instantiate model
+(define model-a (new ngram-model (n 2)))
+(define model-b (new ngram-model (n 1)))
+
+; Train model on input data
+(send model-a train "../data/greet.txt")
+
+; Predict probabiity of a sentence
+(newline)
+(display "<probability>")
+(newline)
+(display (send model-a probability "Cher read a book"))
+(newline)
+(display (send model-a probability "Cher read a book" 'smooth 'additive 1))
+(newline)
+
+; Generate a random sequence of text
+(newline)
+(display "<generate>")
+(newline)
+(display (send model-a generate 10 '("<s>")))
+(newline)
+
+; Get frequencies
+(newline)
+(display "<get-frequencies>")
+(newline)
+(display (send model-a get-frequency '("John" "read") 'smooth 'additive 1))
+(newline)
+(display (send model-a get-frequency '("Cher" "read") 'smooth 'additive .03))
+(newline)
+(display (send model-a get-frequency '("a" "book")))
+(newline)
+(display (send model-a get-frequency '("John") 'smooth 'additive 1))
+(newline)
